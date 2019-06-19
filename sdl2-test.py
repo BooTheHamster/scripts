b8e9bf44eaf7ff763236d248793e8fd599f97472
Oleg Sevostyanov <oleg.sev@gmail.com>
2019-06-19 16:34:39 +0400

    Добавлены классы для отображения спрайтов в тест SDL2

--- sdl2-test.py
+++ sdl2-test.py
@@ -10,13 +10,62 @@ os.environ['PYSDL2_DLL_PATH'] = str(Path(sys.argv[0]).parent)
 import sdl2.ext  # nopep8
 
 
-class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
+class Renderer(sdl2.ext.SoftwareSpriteRenderSystem):
     def __init__(self, window):
-        super(SoftwareRenderer, self).__init__(window)
+        super(Renderer, self).__init__(window)
 
-    def render(self, components):
+    def render(self, sprites, x=None, y=None):
         sdl2.ext.fill(self.surface, sdl2.ext.Color(0, 0, 0))
-        super(SoftwareRenderer, self).render(components)
+        super(Renderer, self).render(sprites)
+
+
+class SpriteFactory(sdl2.ext.SpriteFactory):
+    def __init__(self):
+        super(SpriteFactory, self).__init__(sdl2.ext.SOFTWARE)
+
+
+class NegativeBlock(sdl2.ext.Entity):
+    _color = sdl2.ext.Color(0, 0, 255)
+
+    def __init__(self, factory, x=0, y=0):
+        self.sprite = factory.from_color(self._color)
+        self.sprite.position = x, y
+
+
+class PositiveBlock(sdl2.ext.Entity):
+    _color = sdl2.ext.Color(255, 0, 0)
+
+    def __init__(self, factory, size, x=0, y=0):
+        self.sprite = factory.from_color(self._color, size)
+        self.sprite.position = x, y
+
+
+class MovementSystem(sdl2.ext.Applicator):
+    def __init__(self, left, top, right, bottom):
+        super(MovementSystem, self).__init__()
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.componenttypes = Velocity, sdl2.ext.Sprite
+
+    def process(self, world, components):
+        for velocity, sprite in components:
+            width, height = sprite.size
+
+            # Спрайты вышедшие за границу видимой области не двигаются.
+            if (sprite.x >= (self.left - width)) and (sprite.x <= self.right):
+                sprite.x += velocity.vx
+
+            if (sprite.y >= (self.top - height)) and (sprite.y <= self.bottom):
+                sprite.y += velocity.vy
+
+
+class Velocity(object):
+    def __init__(self):
+        super(Velocity, self).__init__()
+        self.vx = 0
+        self.vy = 0
 
 
 class Game:
@@ -33,8 +82,12 @@ class Game:
         window.show()
 
         world = sdl2.ext.World()
-        renderer = SoftwareRenderer(window)
-        #world.add_system(renderer)
+        factory = SpriteFactory()
+
+        positive_block = PositiveBlock(world, factory, (20, 20))
+
+        renderer = Renderer(window)
+        world.add_system(renderer)
 
         running = True
         while running:
